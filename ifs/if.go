0e97967dcc0664412962c86542856ba14233cf9e
daveym <davey.mcglade@gmail.com>
2016-02-19 16:15:12 +0000

    if fun

---
+++ ifs/if.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	// limit random to only within the scope if the if statement
+	if random := rand.Intn(10); random < 5 {
+		fmt.Println("your number is less than 5")
+	} else {
+		fmt.Println("your number is greater than 5")
+	}
+
+}
