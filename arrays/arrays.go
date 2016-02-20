6553ba5d00a034a10e6a664b48bc812ac9f19dba
daveym <davey.mcglade@gmail.com>
2016-02-20 12:30:19 +0000

    moar arrays

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -7,13 +7,17 @@ import (
 func main() {
 
 	var a = [5]int{1, 2, 3, 4, 5}
-	var b = [5]rune{'a', 'b', 'c', 'd', 'e'}
+	var b = [5]byte{'a', 'b', 'c', 'd', 'e'}
 
 	fmt.Println(len(a))
 
 	for i := 0; i < len(a); i++ {
 		fmt.Println(a[i])
-		fmt.Println(b[i])
+		fmt.Println(string(b[i]))
 	}
+
+	// multidimensional, woooooo
+	c := [3][3]int{{0, 0, 0}, {0, 0, 0}}
+	fmt.Println(c)
 
 }
