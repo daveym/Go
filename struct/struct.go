79bb5328661afeb06bd03f4f4b2a603ab7e918f6
daveym <davey.mcglade@gmail.com>
2016-02-21 09:25:12 +0000

    nested structs

--- struct/struct.go
+++ struct/struct.go
@@ -8,6 +8,11 @@ type point struct {
 	x, y int
 }
 
+type multipoint struct {
+	name string
+	point
+}
+
 func main() {
 
 	var p point
@@ -19,4 +24,15 @@ func main() {
 	p2 := point{5, 6}
 	fmt.Println(p2)
 
+	// named initialisation
+	p3 := point{x: 1}
+	fmt.Println(p3)
+
+	// nested struct
+	first := multipoint{
+		"first",
+		p2,
+	}
+
+	fmt.Println(first)
 }
